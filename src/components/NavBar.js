58f627d6d09ef9c8638bd286ee4cb9ca4709e46e
belhadj14 <belhadj14@gmail.com>
2025-10-22 15:52:30 -0700

    'p5'

---
+++ src/components/NavBar.js
@@ -0,0 +1,59 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Container, Nav, Navbar} from 'react-bootstrap';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
+const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('Home');
+    const [scrolled, setScrolled] = useState(false);
+ useEffect(() => {
+    
+    const onScroll= () => {
+        if(window.scrollY>50){
+            setScrolled(true)
+        }else
+        {
+            setScrolled(false)
+        }
+    };
+    window.addEventListener("scroll",onScroll)
+
+    return ()=>window.removeEventListener("scroll",onScroll)
+ }, []);
+
+ const onUpdateActiveLink=(value)=>{
+    setActiveLink(value)
+ }
+  
+  return (
+   <Navbar expand="lg" className={scrolled? scrolled:""}>
+      <Container>
+        <Navbar.Brand href="#home">
+            <img src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <span className='navbar-toogler-icon'></span>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={activeLink==='home'?'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLink}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink==='skills'?'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLink}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink==='projects'?'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLink}>Projects</Nav.Link>
+            
+          </Nav>
+          <span className='navbar-text'>
+            <div className='social-icon'></div>
+            <a href="#"><img src={navIcon1} alt="" /></a>
+            <a href="#"><img src={navIcon2} alt="" /></a>
+            <a href="#"><img src={navIcon3} alt="" /></a>
+          </span>
+          <button className='vvd' onClick={()=>console.log("connect")}><span>Let's connect</span></button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
