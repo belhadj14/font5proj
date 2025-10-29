4c4374b39d890c0f1173798860c2d828ccc33005
belhadj14 <belhadj14@gmail.com>
2025-10-29 16:30:26 -0700

    p8

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
-import Banner from './components/Banner';
-import NavBar from './components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import Banner from "./components/Banner";
+import NavBar from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Banner/>
-      
+      {/* <NavBar /> */}
+      <Banner />
     </div>
   );
 }
