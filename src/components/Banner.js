e47533c3879232b165cdd87743da9624ef7c05a2
belhadjt <belhadjt@gmail.com>
2025-10-31 14:37:59 +0100

    task6_2

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,19 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { ArrowRightCircle } from "react-bootstrap-icons";
-import HeaderImage from "../assets/img/header-img.svg";
-import { useEffect, useState } from "react";
-const Banner = () => {
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
-  const [text, setText] = useState("");
-  const [delta, setDelta] = useState(300-Math.random()*100);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Web Designer", "React Developer" ];
   const period = 2000;
+
   useEffect(() => {
     let ticker = setInterval(() => {
       tick();
@@ -17,51 +22,56 @@ const Banner = () => {
     return () => { clearInterval(ticker) };
   }, [text])
 
-  const tick=()=>{
-    let i= loopNum % toRotate.length
-    let fullText=toRotate[i]
-    let updatedText=isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
-    if (isDeleting){
-      setDelta(prevDelta=>prevDelta/2)
-    }
-    if(!isDeleting && updatedText===fullText){
-      setIsDeleting(true);
-      setDelta(period)
-    } else if(isDeleting && updatedText===''){
-      setIsDeleting(false);
-      setLoopNum(loopNum+1)
-      setDelta(500)
 
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
     }
   }
 
   return (
     <section className="banner" id="home">
       <Container>
-        <Row className="align-items-center">
+        <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome To My Portfolio</span>
-            <h1>
-              {`Hi i'm Belhadj`}
-              <span>{text}</span>
-            </h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non
-              sapiente expedita nostrum deserunt tempora dolor.
-            </p>
-            <button onClick={() => console.log("connect")}>
-              Let's connect <ArrowRightCircle size={25} />
-            </button>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm AbdeRahmane Ali Seddik: `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>I am a passionate web designer and React developer dedicated to crafting modern, responsive, and user-focused web experiences. I turn ideas into interactive digital solutions using React, combining creativity with clean, efficient code. My focus is on building scalable web applications that perform beautifully while maintaining a seamless user experience. By bridging the gap between design and development, I create elegant interfaces that transform complex problems into simple, intuitive solutions. With a strong eye for detail and a love for innovation, I aim to deliver digital products that are both functional and visually engaging.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src={HeaderImage} alt="Header Image" />
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
     </section>
-  );
-};
-
-export default Banner;
+  )
+}
